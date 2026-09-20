8b3361b24b213dda46123808df8a3c54dcd56a41
Dan <74341742+t109369039@users.noreply.github.com>
2026-09-20 20:44:05 +0800

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='bangkok-trip-2026-v2';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
